2637abacc49c29e8d8582144498e884898c53e76
Caleb Martinez <cmart041@ucr.edu>
2014-12-05 00:54:37 -0800

    Added core file

---
+++ swimgen.rb
@@ -0,0 +1,16 @@
+module Strokes
+  FLY = 0
+  BACK = 1
+  BREAST = 2
+  FREE = 3
+end
+
+def GenSet(lapsPerRep, maxLaps)
+  numReps = maxLaps / lapsPerRep
+  
+end
+
+subset = [1,2,3]
+
+puts subset[0]
+puts Random.rand(10)
