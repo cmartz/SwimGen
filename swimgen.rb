87dc78530f66eb6dd4519c57561e1b935e82fb6e
Caleb Martinez <cmart041@ucr.edu>
2014-12-06 20:49:37 -0800

    Started implementation of QuickSet method

--- swimgen.rb
+++ swimgen.rb
@@ -1,16 +1,55 @@
-module Strokes
-  FLY = 0
-  BACK = 1
-  BREAST = 2
-  FREE = 3
+
+
+$strokes = {:fly => 0, :back => 1, :breast => 2, :free => 3}
+$strokes_lookup = $strokes.invert
+
+
+def GenSet(lapsPerRep, maxLaps, lapSet)
+  numReps = (maxLaps / lapsPerRep).floor
+
+  set = lapSet.repeated_permutation(lapsPerRep).to_a
+  return set
+
+  #To-Do: implement total distance restrictions
 end
 
-def GenSet(lapsPerRep, maxLaps)
-  numReps = maxLaps / lapsPerRep
-  
+def QuickSet
+  fiveHundredFree = Array.new(20, $strokes[:free])
+  basicWarmup = [fiveHundredFree]
+  techWarmup = GenSet(3, 500, [$strokes[:free], $strokes[:back]])
+  warmup = [basicWarmup, techWarmup]
+
+  hundredFree = Array.new(4, $strokes[:free])
+  mainSet = Array.new(10, hundredFree)
+
+  warmDown = Array.new(20, $strokes[:free])
+
+  set = {:warmup => warmup, :main => mainSet, :cooldown => warmDown}
+  return set
 end
 
-subset = [1,2,3]
+def PrintSet(set)
+  puts "Warm Up: "
+  set[:warmup].each do |warmupSet|
+    #puts "\t" + (subset.size * 25).to_s + "yds:"
+    puts "\t" + warmupSet.size.to_s + " * "
+    warmupSet.each do |subset|
+    end
+  end
 
-puts subset[0]
+  puts "Main Set: "
+  set[:main].each do |subset|
+    #puts "A set!"
+  end
+
+  puts "Cool Down: "
+  set[:cooldown].each do |subset|
+    #puts "A set!"
+  end
+end
+
+set = QuickSet()
+
+PrintSet(set)
+
 puts Random.rand(10)
