a0d01cb586446b69939669c2e7e0c79c4ecd08a1
Caleb Martinez <cmart041@ucr.edu>
2014-12-06 22:33:04 -0800

    Implemented PrintSet and SetToSTring methods.

--- swimgen.rb
+++ swimgen.rb
@@ -21,11 +21,30 @@ def QuickSet
 
   hundredFree = Array.new(4, $strokes[:free])
   mainSet = Array.new(10, hundredFree)
+  main = [mainSet]
 
-  warmDown = Array.new(20, $strokes[:free])
+  warmDownSet = [fiveHundredFree]
+  coolDown = [warmDownSet]
 
-  set = {:warmup => warmup, :main => mainSet, :cooldown => warmDown}
+  set = {:warmup => warmup, :main => main, :cooldown => coolDown}
   return set
+end
+
+def SetToString(set)
+  setString = ""
+  last_lap = nil
+  lap_count = 0
+  set.each do |lap|
+    if lap == last_lap or last_lap == nil
+      lap_count += 1
+    else
+      setString += (lap_count * 25).to_s + " " + $strokes_lookup[last_lap].to_s
+      lap_count = 1
+    end
+    last_lap = lap
+  end
+  setString += (lap_count * 25).to_s + " " + $strokes_lookup[last_lap].to_s
+  return setString
 end
 
 def PrintSet(set)
@@ -34,17 +53,34 @@ def PrintSet(set)
     #puts "\t" + (subset.size * 25).to_s + "yds:"
     puts "\t" + warmupSet.size.to_s + " * "
     warmupSet.each do |subset|
+      puts "\t\t" + (subset.size * 25).to_s + ": "
+      last_lap = nil
+      lap_count = 0
+      subset.each do |lap|
+        if lap == last_lap or last_lap == nil
+          lap_count += 1
+        else
+          puts "\t\t\t" + (lap_count * 25).to_s + " " + $strokes_lookup[last_lap].to_s
+          count = 1
+        end
+        last_lap = lap
+      end
+      puts "\t\t\t" + (lap_count * 25).to_s + " " + $strokes_lookup[last_lap].to_s
     end
   end
 
   puts "Main Set: "
-  set[:main].each do |subset|
-    #puts "A set!"
+  set[:main].each do |mainSet|
+    mainSet.each do |subset|
+      puts "\t\t" + (subset.size * 25).to_s + ": " + SetToString(subset)
+    end
   end
 
   puts "Cool Down: "
-  set[:cooldown].each do |subset|
-    #puts "A set!"
+  set[:cooldown].each do |cooldownSet|
+    cooldownSet.each do |subset|
+      puts "\t\t" + (subset.size * 25).to_s + ": " + SetToString(subset)
+    end
   end
 end
 
