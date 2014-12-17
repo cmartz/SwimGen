01f3a4c6965311594928449c786efc9b233adec7
Caleb Martinez <cmart041@ucr.edu>
2014-12-17 13:34:35 -0800

    Added lap class

---
+++ block.rb
@@ -0,0 +1,11 @@
+class Lap
+{
+
+  def initialize(name,stroke,category,tempo_interval)
+    @name = name
+    @stroke = stroke
+    @category = category
+    @tempo_interval = tempo_interval
+  end
+
+}
